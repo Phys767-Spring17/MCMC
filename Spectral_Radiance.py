135a4b1c6492418dcbfc3bcdf25c28f79df4e382
Misha Savchenko <savchenkmisha@gmail.com>
2017-03-20 18:44:02 -0400

    minor visuals

--- Spectral_Radiance.py
+++ Spectral_Radiance.py
@@ -9,6 +9,9 @@ from matplotlib.ticker import MaxNLocator
 from scipy.optimize import curve_fit
 import PyAstronomy
 
+np.seterr(divide='ignore', invalid='ignore')
+np.seterr(over='ignore', invalid='ignore')
+
 h = 6.626070040*(10**(-34))  #J*s
 c = 299792458 #m/s
 k_b = 1.38064852*(10**(-23))  #m^2*kg*s^-2*K^-1
@@ -30,44 +33,25 @@ def planck(wav, T):
     b = h*c/(wav*k*T)
     intensity = a/ ( (wav**5) * (np.exp(b) - 1.0) )
     return intensity
-"""
+
 X = np.linspace(0,3,256,endpoint=True)
 
 Y1_A = (planck(X*10**-6,3000))
 Y2_A = (planck(X*10**-6,4000))
 Y3_A = (planck(X*10**-6,5000))
 
-plot1, = pl.plot(X,Y1_A, color="red", label='3000 K')
-plot2, = pl.plot(X,Y2_A, color="green", label='4000 K')
-plot3, = pl.plot(X,Y3_A, color="blue", label='5000 K')
-pl.legend([plot1,plot2,plot3],['3000 K','4000 K','5000 K'])
+T1 = 3000
+T2 = 4000
+T3 = 5000
+
+plot1, = pl.plot(X,Y1_A, color="red", label=str(T1)+"K")
+plot2, = pl.plot(X,Y2_A, color="green", label=str(T2)+"K")
+plot3, = pl.plot(X,Y3_A, color="blue", label=str(T3)+"K")
+pl.legend([plot1,plot2,plot3],[str(T1)+"K",str(T2)+"K",str(T3)+"K"])
 pl.xlabel("Wavelength (um)")
 pl.ylabel("Spectral Radiance")
 
 pl.show()
-"""
-X = np.linspace(3,23,256,endpoint=True)
 
-T1 = 25
-T2 = 4000
-T3 = 5000
-
-Y1_B = (np.log10(planck(X*10**-6,T1)))
-Y2_B = (np.log10(planck(X*10**-6,T2)))
-Y3_B = (np.log10(planck(X*10**-6,T3)))
-
-x = np.asarray([3.368,4.618,12.082,22.194,3.6,4.5])
-y = np.asarray([-18.435,-17.042,-15.728,-16.307,-18.063,-17.173])
-yerr = np.asarray([0.087,0.087,0.087,0.087,0.043,0.043])
-
-pl.errorbar(x, y, yerr=yerr, fmt=".k")
-
-#plot1B, = pl.plot(X,Y1_B, color="red", label='3000 K')
-
-#plot2B, = pl.plot(X,Y2_B, color="green", label='4000 K')
-#plot3B, = pl.plot(X,Y3_B, color="blue", label='5000 K')
-#pl.legend([plot1B],['3000 K'])
-pl.xlabel("Wavelength (um)")
-pl.ylabel("LOG10(Spectral Radiance)")
 
 pl.show()
